033e57b8b428596c40c64c0e345865a938a918c6
Atul Varma <varmaa@gmail.com>
2015-03-12 08:57:35 -0400

    use browserid login for admin.

--- teach/urls.py
+++ teach/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django_browserid.admin import site as browserid_admin
+browserid_admin.copy_registry(admin.site)
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'teach.views.home', name='home'),
@@ -8,5 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^$', 'example.views.login'),
     url(r'', include('django_browserid.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(browserid_admin.urls)),
 )
