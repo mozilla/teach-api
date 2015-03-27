2c1369f389c1fd9cf1e8d81243c07a06ece2f060
iamjessklein <jessicabrookeklein@gmail.com>
2015-03-26 21:03:48 -0400

    Copy changes to transactional email
    This fixes https://github.com/mozilla/teach.webmaker.org/issues/393

--- clubs/email.py
+++ clubs/email.py
@@ -5,14 +5,14 @@ CREATE_MAIL_BODY = """\
 
 You did it! You put your city on the map and showed your dedication to increasing web literacy around the world. This is a big first step, so thank you for all that you are doing to teach the web.
 
-Whether you are a new club leader or an expert at running clubs, we recommend you get started by meeting others in our community. Join the conversation on our discussion forum [http://discourse.webmaker.org/category/clubs], or tell us what you're teaching on Twitter using hashtag #teachtheweb.
+Whether you are a new club leader or an expert at running clubs, we recommend you get started by meeting others in our community. Join the conversation in our discussion forum [http://discourse.webmaker.org/category/clubs], or tell us what you're teaching on Twitter using hashtag #teachtheweb.
 
 A member of our team will send you an email soon to see how you're doing and make sure your club is on the road to success!
 
-Michelle Thorne
+Michelle Thorne,
 on behalf of the Mozilla Learning team
 
-p.s. Be sure to check out our tips for running clubs [link to page that will be built in #255]. They'll help you get support, find training opportunities, increase the impact of your club, and much more. 
+P.S. Be sure to check out our Clubs Toolkit [http://teach.mozilla.org/clubs-toolkit] to help you get support, find training opportunities, increase the impact of your club, and much more.
 """
 
 CREATE_MAIL_STAFF_SUBJECT = 'A new Club has been added to the map'
