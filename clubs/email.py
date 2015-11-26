1e643583e8b8756c9f1f4cf9fb81cc5f701eca23
Mavis Ou <mmmavisss@gmail.com>
2015-11-26 13:17:07 -0800

    Fixes #22 - changed email copy

--- clubs/email.py
+++ clubs/email.py
@@ -3,22 +3,13 @@ CREATE_MAIL_SUBJECT = 'Thanks for joining the global movement to teach the web!'
 CREATE_MAIL_BODY = """\
 %(username)s,
 
-Thanks for your interest in teaching the web!
+Thanks for your interest in teaching the web! We are thrilled that you have taken the first step in your journey to becoming a Mozilla Club Captain.
 
-Get started right away by guiding a group of learners through these featured activities: %(TEACH_SITE_URL)s/activities/
+In order to better understand your plans and needs for your club, we'd like to know more about you. Please provide brief answers (2-3 sentences) to the questions in this survey http://bit.ly/1MP9BzB. Your answers will help us in the process of reviewing/approving your submission on the Mozilla Clubs map. Note, it is mandatory to complete and return these questions in order to proceed to the next step.
 
-We believe in the power of peer learning. That's why we match each Mozilla Club Captain with a volunteer Regional Coordinator who can support you in getting started and making the most of this program.
+We look forward to hearing from you! You can tweet to us @mozteach or using hashtag #teachtheweb if you need any help.
 
-Please note: Our first cohort of Regional Coordinators is in full swing right now, so you've been added to our waiting list. We'll match you with a Regional Coordinator as soon as we can. Your club will not show up on the map until you've been matched up.
-
-To begin on your own, check out these resources to organize and teach your learners: http://mozilla.github.io/learning-networks/clubs/#resources
-
-Please also say hello in our discussion forum (http://discourse.webmaker.org/category/clubs) or tweet using hashtag #teachtheweb if you need any help.
-
-Let's teach the web!
-
-Michelle Thorne
-on behalf of the Mozilla Learning Network
+Mozilla Clubs team
 """
 
 CREATE_MAIL_STAFF_SUBJECT = 'Please review: new club has been submitted'
