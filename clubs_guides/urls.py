ae1811bc3c70e2f65682c70ed7d6982be55b0d28
Gideon Thomas <r.gideonthomas@gmail.com>
2016-08-02 10:57:25 -0400

    Fix #43 - Add scaffolding for clubs guides app (#52)

---
+++ clubs_guides/urls.py
@@ -0,0 +1,3 @@
+from django.conf.urls import url
+
+# Create you url mappings here.
--- teach/settings.py
+++ teach/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'clubs',
+    'clubs_guides',
     'groups',
     'credly'
 )
